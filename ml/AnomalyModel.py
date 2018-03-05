9085b815638330575aa74fcf0a8ad2cf88f1b85c
Karan Singhal <karan1149@gmail.com>
2018-03-04 19:15:26 -0800

    Model using IsolationForest, without validation

--- ml/AnomalyModel.py
+++ ml/AnomalyModel.py
@@ -1,24 +1,38 @@
-
+import sklearn.ensemble.IsolationForest
+from sklearn.externals import joblib
+import numpy as np
 DEBUG = True
 
 class AnomalyModel(object):
   def __init__(self):
     """
     """
-    pass
+    self.model = IsolationForest()
 
   def predict(self, packet):
     """
     Predicts whether the given packet is anomalous.
+    Dimensionality of packet is (n_features), +1 if inlier, -1 if anomaly
     """
-    pass
+    return self.model.predict([packet])[0]
+
+
+  def predicts(self, packets):
+    """
+    Predicts whether multiple packets are anomalous.
+    Dimensionality of packets is (n_samples, n_features).
+    Returns predictions with dims (n_samples)
+    +1 if inlier, -1 if anomaly
+    """
+    return self.model.predict(packets)
 
 
   def fit(self, packets):
     """
     Trains the underlying model using an unlabeled list of packets.
+    Packets has dimensionality (n_samples, n_features)
     """
-    pass
+    self.model.fit(packets)
 
   def validation(self, packets, labels):
     """
@@ -26,20 +40,27 @@ class AnomalyModel(object):
     for the trained model.
     - accuracy
     - precision
+    - recall
     - f1 score
     - false negative rate
     - false positive rate
     """
-    pass
+    predictions = self.model.predict(packets)
+    labels = np.array(labels)
+    assert(predictions.shape == labels.shape)
+    
+    accuracy = np.mean(labels == predictions)
+    
+    
 
   def save(self, path):
     """
     Dumps the model to the given path.
     """
-    pass
+    joblib.dump(self.model, path)
 
   def load(self, path):
     """
     Loads a model dump from path and initializes class members.
     """
-    pass
+    self.model = joblib.load(path)
