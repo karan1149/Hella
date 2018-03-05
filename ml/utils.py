e072c47c13c056fdd008bd1bb7465cbf25cd84ac
jkamalu <john.kamalu@gmail.com>
2018-03-04 20:24:24 -0800

    Added initial featurization functionality sans dest, src, and payload fields for eth, ip, and tcp objects.

--- ml/utils.py
+++ ml/utils.py
@@ -1,5 +1,8 @@
 import dpkt
 
+IP_FEATURES = ['len', 'id', 'off', 'ttl', 'p', 'sum']
+TCP_FEATURES = ['sport', 'dport', 'seq', 'ack', 'flags', 'win', 'sum']
+
 def read_tcpdump_file(tcpdump_file):
   """
   Reads the tcpdump file and returns a list of packets (bytes).
@@ -14,10 +17,29 @@ def featurize_packets(packets):
   """
   for ts, buf in packets:
     eth = dpkt.ethernet.Ethernet(buf)
-    ip = eth.data
-    tcp = ip.data
-    # if len(tcp.data) > 0 and tcp.dport == 80:
-    #   http = dpkt.http.Request(tcp.data)
-    # else:
-    #   http = b''
-    yield ts, eth, ip, tcp
+
+    packets = [ts]
+
+    try:
+      ip = eth.data
+      for key in IP_FEATURES:
+        packets.append(ip[key])
+    except:
+      print("Exception in IP parse")
+      continue
+
+    try:
+      tcp = ip.data      
+      for key in TCP_FEATURES:
+        packets.append(tcp[key])
+    except:
+      print("Exception in TCP parse")
+      continue
+
+    yield packets
+
+if __name__ == "__main__":
+  packets = read_tcpdump_file("outside.tcpdump")
+  featurizer = featurize_packets(packets)
+  for f in featurizer:
+    print(f)
