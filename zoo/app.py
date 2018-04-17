884ed018025750c272f00347ae6920c2a5a82c42
Karan Singhal <karan1149@gmail.com>
2018-04-17 11:35:34 -0700

    Added template loading and prediction logic to web app

--- zoo/app.py
+++ zoo/app.py
@@ -1,7 +1,11 @@
 import flask
 from flask import Flask, jsonify, request, render_template
 import os
+import sys
+sys.path.append("../ml")
 from anomaly_model import AnomalyModel
+sys.path.append("../monitor")
+import test_data
 import pickle
 
 dataset_dir = 'datasets/'
@@ -12,14 +16,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-	dataset_names = [name[:-4] for name in os.listdir(dataset_dir) if name.endswith('.pkl')]
-	model_names = [name[:-4] for name in os.listdir(model_dir) if name.endswith('.pkl')]
+	# Take all files in the appropriate directories matching ".pkl"
+	dataset_names = [make_name_pretty(name) for name in os.listdir(dataset_dir) if name.endswith('.pkl')]
+	model_names = [make_name_pretty(name) for name in os.listdir(model_dir) if name.endswith('.pkl')]
 	return render_template('index.html', dataset_names=dataset_names, model_names=model_names)
 
 @app.route('/predict', methods=['POST'])
 def predict():
 	params = request.get_json(silent=True, force=True)
-	model_path = model_dir + "isolation_forest.pkl"
+	model_path = model_dir + "isolation_forest_contextual.pkl"
 	dataset_path = dataset_dir + "darpa_1000.pkl"
 
 	model = AnomalyModel()
@@ -28,10 +33,30 @@ def predict():
 	with open(dataset_path, 'r') as f:
 		test_data = pickle.load(f)
 
-	
+	X_raw = [dp.pkt for dp in test_data.dps]
+	X = model.featurizer(X_raw)
+	print(len(X))
+	Y = [1 if dp.malicious else 0 for dp in test_data.dps]
 
-	return jsonify({})
+	pred = model.predicts(X)
 
+	metrics = model.validation(pred, Y)
+	print(metrics)
+
+	return jsonify(metrics)
+
+# Take a model/dataset name and "prettify" it
+# by replacing underscores with spaces and using titlecase
+def make_name_pretty(name):
+	name = name[:-4]
+	name = name.replace("_", " ")
+	name = name.title()
+	return name
+
+# Reverse the above process for a single name
+def make_name_ugly(name):
+	name = name.replace(" ", "_").tolower() + ".pkl"
+	return name
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000, debug=True)
