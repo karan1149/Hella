fd439aa158b72ada78388494d83bed4a20e1f137
jkamalu <john.kamalu@gmail.com>
2018-04-24 16:21:46 -0700

    CountBasedFeaturizer and TimeBaseFeaturizer
    Two classed extend from base class which does basic featurization.
    Basic featurization controlled with Enum.
    Enhanced featurization currently not controlled by Enum. Currently just counts in context window.

--- ml/featurizer.py
+++ ml/featurizer.py
@@ -1,64 +1,190 @@
 from collections import deque, defaultdict
 from enum import Enum, unique
 
+from scapy.all import *
+from utils import *
+
 import time
 
-# IP_FEATURES = ['len', 'id', 'off', 'ttl', 'p', 'sum']
-# TCP_FEATURES = ['sport', 'dport', 'seq', 'ack', 'flags', 'win', 'sum']
+IP_HEADER = ['len', 'id', 'frag', 'ttl', 'proto']
+TCP_HEADER = ['sport', 'dport', 'seq', 'ack', 'flags', 'window']
 
 class BasicFeaturizer:
 
-	def __init__(self):
-		pass
+    def __init__(self):
 
-	def featurize(self, pkt):
-		"""
-		Converts a scapy packet into a list of features.
-		"""
-		features = [pkt.time]
-		if IP in pkt:
-		features.extend([pkt[IP].len, pkt[IP].id, pkt[IP].frag, \
-			pkt[IP].ttl, pkt[IP].proto, pkt[IP].chksum])
-		if TCP in pkt:
-		features.extend([pkt[TCP].sport, pkt[TCP].dport, pkt[TCP].seq, \
-			pkt[TCP].ack, pkt[TCP].flags, pkt[TCP].window, pkt[TCP].chksum])
-		return features
+        self.BasicFeatures = self._feature_enum()
 
-class TimeBasedFeaturizer():
+    def _feature_enum(self):
+        """
+        Returns an Enum mapping 'time' and all features in IP_HEADER and TCP_HEADER
+        to a range of values beginning at 0
+        """
 
-	def __init__(self, window=50):
+        return Enum('Features', { feat : i for i, feat in enumerate(['time'] + IP_HEADER + TCP_HEADER) })
 
-		self.window = window
-		self.pkt_history = deque()
+    def featurize(self, raw_pkt, timestamp=None):
+        """
+        Returns a list of basic features as defined by the BasicFeatures enum
 
-		self.features = Enum('Features', ['time'] + IP_FEATURES + TCP_FEATURES)
+        raw_pkt: The scapy Packet_metaclass instance to be converted
+        timestamp: the artificial timestamp (useful for preexisting, converted packets)
+        """
 
-		self.ip_feature_stats = { key : defaultdict(int) for ip_feature in IP_FEATURES }
-		self.tcp_feature_stats = { key : defaultdict(int) for tcp_feature in TCP_FEATURES }
+        features = [None for i in self.BasicFeatures]
 
-	def _cull_history(self, now):
+        if timestamp:
+            features[self.BasicFeatures['time'].value] = timestamp
+        else:
+            features[self.BasicFeatures['time'].value] = raw_pkt.time
 
-		while now - self.pkt_history[0][0] > self.window * 1000:
+        if IP in raw_pkt:
+            for feat in IP_HEADER:
+                features[self.BasicFeatures[feat].value] = getattr(raw_pkt[IP], feat)
 
-			trash_pkt = self.pkt_history.popleft()
+        if TCP in raw_pkt:
+            for feat in TCP_HEADER:
+                features[self.BasicFeatures[feat].value] = getattr(raw_pkt[TCP], feat)
 
-			self.ip_feature_stats[trash_pkt]
+        return features
 
-	def _increment_history(self, pkt):
+class CountBasedFeaturizer(BasicFeaturizer):
 
-		self.pkt_history.append(pkt)
+    def __init__(self, pkt_window=50):
 
-	def _featurize_basic(self, pkt):
-		features = [pkt.time]
-		if IP in pkt:
-		features.extend([pkt[IP].len, pkt[IP].id, pkt[IP].frag, \
-			pkt[IP].ttl, pkt[IP].proto, pkt[IP].chksum])
-		if TCP in pkt:
-		features.extend([pkt[TCP].sport, pkt[TCP].dport, pkt[TCP].seq, \
-			pkt[TCP].ack, pkt[TCP].flags, pkt[TCP].window, pkt[TCP].chksum])
-		return features
+        self.pkt_window = pkt_window
+        self.pkt_history = deque()
+
+        self.BasicFeatures = super(type(self), self)._feature_enum()
+
+        self.feature_stats = { feat : defaultdict(int) for feat in self.BasicFeatures }
+
+    def _cull_history(self):
+        """
+        Removes old featurized packets from self.pkt_history
+        Updates self.feature_stats
+        """     
+
+        if len(self.pkt_history) > self.pkt_window:
+
+            trash_pkt = self.pkt_history.popleft()
+
+            for feat in self.BasicFeatures:
+
+                self.feature_stats[feat][trash_pkt[feat.value]] -= 1
+
+    def _increment_history(self, pkt):
+        """
+        Adds the featurized packet to the self.pkt_history
+        Updates self.feature_stats
+        """
+
+        self.pkt_history.append(pkt)
+
+        for feat in self.BasicFeatures:
+
+            self.feature_stats[feat][pkt[feat.value]] += 1  
+
+    def _update_history(self, pkt):
+        """
+        Increments and culls packet history and feature stats
+        """
+
+        self._increment_history(pkt)
+        self._cull_history()
+
+    def featurize(self, raw_pkt):
+
+        pkt = super(type(self), self).featurize(raw_pkt)
+
+        self._update_history(pkt)
+
+        pkt.extend([self.feature_stats[self.BasicFeatures(i)][pkt[i]] for i in range(len(pkt))])
+
+        return pkt
 
 
-	def featurize(self, pkt):
-		pass
+class TimeBasedFeaturizer(BasicFeaturizer):
+
+    def __init__(self, sec_window=10):
+
+        self.sec_window = sec_window
+        self.pkt_history = deque()
+
+        self.BasicFeatures = super(type(self), self)._feature_enum()
+
+        self.feature_stats = { feat : defaultdict(int) for feat in self.BasicFeatures }
+
+    def _cull_history(self, pkt):
+        """
+        Removes old featurized packets from self.pkt_history
+        Updates self.feature_stats
+        """     
+
+        now = pkt[self.BasicFeatures.time.value]
+
+        while now - self.pkt_history[0][self.BasicFeatures.time.value] > self.sec_window:
+
+            trash_pkt = self.pkt_history.popleft()
+
+            for feat in self.BasicFeatures:
+
+                self.feature_stats[feat][trash_pkt[feat.value]] -= 1
+
+    def _increment_history(self, pkt):
+        """
+        Adds the featurized packet to the self.pkt_history
+        Updates self.feature_stats
+        """
+
+        self.pkt_history.append(pkt)
+
+        for feat in self.BasicFeatures:
+
+            self.feature_stats[feat][pkt[feat.value]] += 1  
+
+    def _update_history(self, pkt):
+        """
+        Increments and culls packet history and feature stats
+        """
+
+        self._increment_history(pkt)
+        self._cull_history(pkt)
+
+    def featurize(self, raw_pkt):
+
+        pkt = super(type(self), self).featurize(raw_pkt)
+
+        self._update_history(pkt)
+
+        pkt.extend([self.feature_stats[self.BasicFeatures(i)][pkt[i]] for i in range(len(pkt))])
+
+        return pkt        
+
+if __name__ == '__main__':
+    reader = read_tcpdump_file('data/week1_friday.tcpdump')
+
+    print('Test CountBasedFeaturizer')
+    CBF = CountBasedFeaturizer(50)
+    pkts_read = 0    
+    while True:
+        pkt_read = reader.__next__()
+        raw_pkt = Ether(pkt_read[1])
+        if IP in raw_pkt and TCP in raw_pkt:   
+            feats = CBF.featurize(raw_pkt)
+            pkts_read += 1
+        if pkts_read > CBF.pkt_window * 2: 
+            break
+
+    print('Test TimeBasedFeaturizer')
+    TBF = TimeBasedFeaturizer(1)
+    start = time.time()
+    while True:
+        pkt_read = reader.__next__()
+        raw_pkt = Ether(pkt_read[1])
+        if IP in raw_pkt and TCP in raw_pkt:   
+            feats = TBF.featurize(raw_pkt)
+        if time.time() - start > TBF.sec_window * 2: 
+            break
+
 
