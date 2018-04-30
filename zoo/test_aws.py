ff966cb92b5fb984c93b400ed77dea9eaee6baf1
Alan <alanf94@stanford.edu>
2018-04-30 14:44:27 -0700

    change to test_aws

--- zoo/test_aws.py
+++ zoo/test_aws.py
@@ -6,7 +6,7 @@ for bucket in s3.buckets.all():
   print(bucket.name)
 
 try:
-    s3.Bucket('seer-model-zoo').download_file(KEY, 'my_local_image.jpg')
+    s3.Bucket('seer-model-zoo').download_file('datasets/', 'my_local_image.jpg')
 except botocore.exceptions.ClientError as e:
     if e.response['Error']['Code'] == "404":
         print("The object does not exist.")
