e0944398fc333437c5632f65f30c9b46fa85aee7
Pratyaksh Sharma <pratyaksh@me.com>
2018-05-15 16:22:45 -0700

    removed duplicate code from simulate script

--- simulation/simulate.py
+++ simulation/simulate.py
@@ -73,37 +73,3 @@ class Simulator():
 
     def send_to_monitor(self, pkt):
         self.monitor.handle_pkt(pkt)
-
-# https://mail.python.org/pipermail/tutor/2013-January/093635.html
-def featurizer_range(arg):
-    try:
-        value = int(arg)
-    except ValueError as err:
-       raise argparse.ArgumentTypeError(str(err))
-
-    if value < 1 or value > 3:
-        message = "Expected 1 <= value <= 3, got value = {}".format(value)
-        raise argparse.ArgumentTypeError(message)
-
-    return value    
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--verbose', action='store_true', default=False, help='the log verbosity')
-
-    parser.add_argument('data_file', help='the .pkl file to read packets from (--train) or Test_data from (--test)')
-    parser.add_argument('model_file', help='the .pkl file to write to (--train) and read from (--test)')
-
-    train_test = parser.add_mutually_exclusive_group(required=True)
-    train_test.add_argument('--train', action='store_true', help='train the model')
-    train_test.add_argument('--test', action='store_true', help='test the model')
-
-    parser.add_argument('--featurizer', default=None, type=featurizer_range, help='1 | 2 | 3')
-
-    args = parser.parse_args()
-
-    if args.featurizer and not args.train:
-        parser.error('Abort: --featurizer is implicit with --test')
-
-    simulator = Simulator(args.model_file, args.data_file, args.train, int(args.verbose), featurizer=featurizer_classes[args.featurizer])
-    simulator.run()
