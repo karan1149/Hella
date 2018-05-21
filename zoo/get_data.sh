217a32f268c0f3dcd92dbe181c5f811c7d77a9bf
Alan <alanf94@stanford.edu>
2018-05-21 12:25:05 -0700

    move get_data and gdrive to top-level zoo

--- zoo/get_data.sh
+++ zoo/get_data.sh
@@ -20,16 +20,24 @@ fi
 
 # Variable names mapping the name of the file to be downloaded
 # with their unique Google Drive ID.
+
+# Data
 api_old_packets_ct79400="1G5h-OH_x9tSp932snjrhMWRT9b87DqN1"
+
+# Models
 isolation_forest_countbased_trainct80k="1LbJv9CxcYTMQOKoSpXre8xm9GP-IdHlI"
 
-to_download=(api_old_packets_ct79400 isolation_forest_countbased_trainct80k)
+to_download_models=(isolation_forest_countbased_trainct80k)
+to_download_data=(api_old_packets_ct79400)
 
 # TODO: make this logic nicer.
-for i in {0..1}
+for i in {0..0}
 do
-  echo "Downloading ${to_download[$i]}.pkl"
-  python gdrive.py ${!to_download[$i]} ${to_download[$i]}.pkl
+  echo "Downloading model ${to_download_models[$i]}.pkl"
+  python gdrive.py ${!to_download_models[$i]} models/${to_download_models[$i]}.pkl
+
+  echo "Downloading data ${to_download_data[$i]}.pkl"
+  python gdrive.py ${!to_download_data[$i]} datasets/${to_download_data[$i]}.pkl
 done
 
 exit 0
