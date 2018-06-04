cc5dbf1935b367975f18326822d6844a93087c77
sktollman <stollman@stanford.edu>
2018-06-03 21:26:02 -0700

    Adds attack types to simulation (#59)
    Also:
    - adds directions to readme
    - optionally save test dataset to pkl file
    - timeout if not all packets are classified

--- simulation/simulate.py
+++ simulation/simulate.py
@@ -9,22 +9,27 @@ from featurizer import BasicFeaturizer, CountBasedFeaturizer, TimeBasedFeaturize
 import api
 import argparse
 from scapy.all import *
+import pickle
+import time
 
 featurizer_classes = { 1: BasicFeaturizer, 2: CountBasedFeaturizer, 3: TimeBasedFeaturizer, 4: BasicFeaturizerUDP}
 
 class Simulator():
 
-    def __init__(self, model_file, data_file, is_training, verbosity, featurizer=None):
+    def __init__(self, model_file, data_file, out_file, attack_type,
+        is_training, verbosity, featurizer=None):
         """
-        This class will eventually simulate real communication between 
+        This class will eventually simulate real communication between
         the api, method, and monitor. This behavior only makes sense for
         when the system is "deployed" aka making requests in real time.
 
-        Currently, it serves to stitch together the method and monitor 
+        Currently, it serves to stitch together the method and monitor
         (train/test and display) of static data.
-        """        
+        """
         self.model_file = model_file
         self.data_file = data_file
+        self.out_file = out_file
+        self.attack_type = attack_type
         self.is_training = is_training
         self.verbosity = verbosity
         self.featurizer = featurizer
@@ -48,16 +53,25 @@ class Simulator():
         data_file:  pcap file to read from
         verbosity:  verbosity
         """
-        self.method = Method(api=self.api, send_fn=self.send_to_monitor)   
+        self.method = Method(api=self.api, send_fn=self.send_to_monitor)
         self.method.load_model(self.model_file)
 
-        self.monitor = Monitor(log_level=self.verbosity, send_fn=self.send_to_method)
-        self.monitor.load_data(self.data_file)
+        self.monitor = Monitor(log_level=self.verbosity, send_fn=self.send_to_method,
+            attack_type=self.attack_type)
+        pkts = []
+        with open(self.data_file, 'rb') as f:
+            pkts = pickle.load(f)
+        self.monitor.create_test_data(pkts)
 
         self.monitor.send()
-        while not self.monitor.completed():
+        start_time = time.time()
+        # time out after 10 seconds
+        while not self.monitor.completed() and time.time() - start_time < 10:
             sleep(.1)
-        self.monitor.show_results() 
+        self.monitor.show_results()
+
+        if self.out_file:
+            pickle.dump(self.monitor.test_data, open(self.out_file, 'wb'))
 
     def run(self):
         """
